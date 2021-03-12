3e9cbeaecb2312454dfc44dec88878beab4cc6a2
Rosangela Kelwattes <l00162027@student.lyit.ie>
2021-03-12 14:55:00 +0000

    q7 - updateItem

---
+++ Task3/Trello.py
@@ -0,0 +1,30 @@
+# @Time    : 07/03/2021 22:38
+# @Author  : Rosangela Kelwattes
+# @Email   : l00162027@student.lyit.ie
+import json
+
+from Task3.Endpoints import Endpoints
+
+
+class Trello:
+    USER_ID = "6043bf62cad238312f4b2af4"
+    teamMemberName = "Tom"
+    name = 'New Test Board12356'
+    desc = 'Board to test board creation'
+
+    # Create board
+    response = Endpoints.createNewBoard(name, desc)
+    data = response.text
+    parsed = json.loads(data)
+    idBoard = parsed['id']
+
+    # get getUserInfo
+    Endpoints.getUserInfo(USER_ID)
+    # get addToDoItems
+    Endpoints.addToDoItems(name, teamMemberName)
+    # update Item
+    Endpoints.updateItem(name, "Key task 1", "complete")
+
+
+
+
